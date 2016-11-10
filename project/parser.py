fdecf73211e84ebc2a57b5f227cdc82dad2cb992
Miro <miro.trifonov@yahoo.com>
2016-11-10 14:28:35 +0000

    Input handling function finished. Experimentation not yet supported

--- project/parser.py
+++ project/parser.py
@@ -1,9 +1,20 @@
+import sys
+# parse_file -> given a filename as an input, parses a bin collection file it into a dictionary,
+# provided it has rightly formatted input and prints error otherwise
+# validate_input -> called automatically by parse_file this function checks if all needed arguments are given
+# and displays warnings for some types of unrealistic input
+# num -> makes sure that given argument is int or a float, as specified by num_type
+
+# TODO add check for correct row length in matrix length
+
 def parse_file(filename):
     arguments = {}
-    expected_global_arguments = ['lorryVolume', 'lorryMaxLoad', 'binServiceTime', 'binVolume', 'disposalDistrRate',
-                                 'disposalDistrShape', 'bagVolume', 'bagWeightMin', 'bagWeightMax', 'noAreas',
-                                 'stopTime', 'warmUpTime']
+    is_valid = True
+    expected_global_arguments_int = ['lorryVolume', 'lorryMaxLoad', 'binServiceTime', 'disposalDistrShape', 'noAreas']
+    expected_global_arguments_float = ['binVolume', 'disposalDistrRate', 'bagVolume', 'bagWeightMin', 'bagWeightMax',
+                                       'stopTime', 'warmUpTime']
     expected_area_arguments = ['serviceFreq', 'thresholdVal', 'noBins']
+    area_ids = []
     try:
         parsed_file = open(filename)
     except IOError as e:
@@ -14,77 +25,106 @@ def parse_file(filename):
     area_id = -1
     for line in parsed_file:
         tokens = map(str.strip, line.split())
-        if tokens[0] is '#':
+        if tokens is [] or tokens[0] == '#':
             continue
-        elif tokens[0] in expected_global_arguments and len(tokens) > 1:
-            key, value = tokens[0], tokens[1]
+        elif road_layout_input == 'expect_arg':
+            if tokens[0] == 'roadsLayout':
+                road_layout_input = True
+                continue
+            else:
+                print "Error: 'roads layout' required"
+                sys.exit(1)
+        elif road_layout_input and remaining_road_matrix_len != 0:
+            new_list = arguments.get('roadsLayout {}'.format(area_id), [])
+            for i in range(1,tokens.__len__()):
+                tokens[i] = num(tokens[i], 'roadsLayout', 'int')
+                if tokens[i] is False:
+                    print "Error \"not an integer\". Possibly caused by missing row in road layout matrix"
+                    sys.exit(1)
+            new_list.append(tuple(tokens))
+            arguments[('roadsLayout {}'.format(area_id))] = new_list
+            remaining_road_matrix_len -= 1
+            if remaining_road_matrix_len == 0:
+                road_layout_input = False
+        elif tokens[0] in expected_global_arguments_int and len(tokens) > 1:
+            key, value = tokens[0], num(tokens[1], tokens[0], 'int')
+            if value is False:
+                sys.exit(1)
             arguments[key] = value
-            expected_global_arguments.remove(key)
+            expected_global_arguments_int.remove(key)
+            if len(tokens) > 2:
+                print "Warning too many arguments given for: " + line
+        elif tokens[0] in expected_global_arguments_float and len(tokens) > 1:
+            key, value = tokens[0], num(tokens[1], tokens[0], 'float')
+            if value is False:
+                sys.exit(1)
+            arguments[key] = value
+            expected_global_arguments_float.remove(key)
             if len(tokens) > 2:
                 print "Warning too many arguments given for: " + line
         elif tokens[0] == 'areaIdx':
             area_id = tokens[1]
+            area_ids.append(area_id)
             for arg in expected_area_arguments:
                 arguments["{} {}".format(arg, area_id)] = tokens[tokens.index(arg) + 1]
             road_layout_input = 'expect_arg'
             remaining_road_matrix_len = int(arguments.get('noBins {}'.format(area_id))) + 1
-        elif road_layout_input is 'expect_arg':
-            road_layout_input = True
-            continue
-        elif road_layout_input and remaining_road_matrix_len != 0:
-            new_list = arguments.get('roadsLayout {}'.format(area_id), [])
-            new_list.append(tuple(tokens))
-            arguments[('roadsLayout {}'.format(area_id))] = new_list
-            remaining_road_matrix_len -= 1
-            if remaining_road_matrix_len is 0:
-                road_layout_input = False
         else:
             print "Invalid input: " + line
+            sys.exit(1)
     parsed_file.close()
-    #is_valid = validateInput(arguments, road_layout_input)
-    is_valid = True
+    expected_global_arguments = expected_global_arguments_int + expected_global_arguments_float
+    is_valid = is_valid and validate_input(arguments, road_layout_input, expected_global_arguments, area_ids)
     if is_valid:
         return arguments
     else:
-        return None#
-# def validateInput(arguments, road_mode_not_finished):
-#     # TODO experimentation mode
-#     return True
-#     # global_arguments = [('lorryVolume', 'int'), ('lorryMaxLoad', 'int'), ('binServiceTime', 'int'),
-#     #                     ('binVolume', 'float'),
-#     #                     ('disposalDistrRate', 'float'), ('disposalDistrShape', 'int'),
-#     #                     ('bagVolume', 'float'), ('bagWeightMin', 'float'), ('bagWeightMax', 'float'),
-#     #                     ('noAreas', 'int'),
-#     #                     ('areaIdx', 'int'), ('serviceFreq', 'float'), ('thresholdVal', 'float'),
-#     #                     ('noBins', 'int'), ('roadsLayout', 'matrix'), ('areaIdx', 'int'), ('stopTime', 'float'),
-#     #                     ('warmUpTime', 'float'), ]
-#     # parse_problems = arguments.__len__() != expexted_args.__len__()
-#     # if parse_problems is True:
-#     #     return "Invalid number of arguments"
-#     #
-#     # for key in expexted_args:
-#     #     argument = arguments.get(key[0])
-#     #     if argument is None:
-#     #         parse_problems = True
-#     #         # will that work for python 2.7
-#     #         print '{} not specified'.format(key[0])
-#     #     else:
-#     #         if key[1] is 'int' and not argument.is_integer():
-#     #             print '{} should be integer'.format(key[0])
-#     #         elif key[1] is 'float' and not isinstance(argument, numbers.Number):
-#     #             print '{} should be float'.format(key[0])
-#     #         elif key[1] is 'matrix' and verifyIntegers(argument):
-#     #             print '{} should be a matrix'  # TODO
-#     #             # Note: the current configuration does not require the parameters to be in correct order, so I'm not checking for that
-#     #
-#     # if parse_problems is True:
-#     #     return True
-#     # else:
-#     #     return False
-#
-#
-# def verify_integers(argument):
-#     for row in argument:
-#         for number in row:
-#             number.isdigit()
+        return None
 
+
+def validate_input(arguments, expect_road_input, remaining_global_arguments,areas):
+    if expect_road_input:
+        print "Error: Road layout matrix missing rows"
+        sys.exit(1)
+    elif remaining_global_arguments:
+        for arg in remaining_global_arguments:
+            print "Error: Expected input: {}".format(arg)
+        sys.exit(1)
+    elif len(areas) is not arguments.get('noAreas'):
+        print "Areas given are less then specified"
+        sys.exit(1)
+
+    # Warnings: Print warning for some common examples of valid, but unrealistic input
+    if arguments.get('lorryVolume') < 2 * arguments.get('binVolume'):
+        print "Warning: lorry volume is small"
+    if arguments.get('binVolume') < 2 * arguments.get('bagVolume'):
+        print "Warning: bin volume is small"
+    if arguments.get('lorryMaxLoad') < 2 * arguments.get('bagWeightMax'):
+        print "Warning: lorry max load capacity is small"
+    for i in areas:
+        if arguments.get('serviceFreq {}'.format(i)) < arguments.get('disposalDistrRate') * arguments.get('bagVolume') / arguments.get('binVolume'):
+            print "Warning: service frequency may be too small for area number {}".format(i)
+    if arguments.get('warmUpTime') > arguments.get('stopTime'):
+        print "Warning: warm up time too big"
+    if arguments.get('binServiceTime') > 300:
+        print "Warning: bin service time may be too big"
+
+    return True
+
+
+def num(s, key, num_type):
+    if num_type is 'int':
+        try:
+            return int(s)
+        except ValueError:
+            print "Error: \"{}\" is not an integer in  {}".format(s, key)
+            return False
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except ValueError:
+            print "Error: \"{}\" is not a number in  {}".format(s, key)
+            return False
+
+parse_file("input_invalid (4).txt")
